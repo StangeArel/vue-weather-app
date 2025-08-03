4f4789908bd97c07c080fa364dc8cbe9b4d9cc88
Valeriya Stange <valeriya.stange@gmail.com>
2025-08-03 21:25:40 +0200

    ✨ Wetter-App Grundstruktur aufgebaut: Stadtname dynamisch mit Input verknüpft & modernes Styling mit Farbverlauf umgesetzt

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,70 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      city: ""
+    };
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrapper">
+    <h1>Wetter-App</h1>
+    <p>Erfahre das aktuelle Wetter in {{ city }} Stadt.</p>
+    <input v-on:input="this.city = $event.target.value" type="text" placeholder="Gib deine Stadt ein" />
+    <button>Wetter anzeigen</button>
+    <p>Hier wird das Wetter angezeigt.</p>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.wrapper {
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  background: linear-gradient(339deg, rgba(137, 248, 250, 0.804) 0%, rgba(197, 146, 190, 0.901) 100%);
+  padding: 20px;
+text-align: center;
+color: #036014;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.wrapper h1 {
+  margin-top: 50px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.wrapper p {
+  margin-top: 20px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.wrapper input {
+margin-top: 30px;
+background: transparent;
+  border: 0;
+  border-bottom: 2px solid #110813;
+  color: #860a6f;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.wrapper input:focus {
+  border-bottom-color: #6e3d7d;
+}
+
+.wrapper button {
+  background: #e3bc4b;
+  color: #531c02;
+  border-radius: 10px;
+  border: 2px solid #b99935;
+  padding: 10px 15px;
+  margin-left: 20px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+
+.wrapper button:hover {
+  transform: scale(1.1) translateY(-5px);
 }
 </style>
